c242b934832b327d2423bb36e87865637e029dff
Julieta Silva <juuli.siilva@gmail.com>
2023-11-06 19:37:30 -0300

    Entregable Clase 4

---
+++ Entregable_Clase_04/README.md
@@ -0,0 +1,23 @@
+# Consigna
+
+## Clases con ECMAScript y ECMAScript avanzado
+
+Realizar una clase “ProductManager”, el cual permitirá trabajar con múltiples productos. Éste debe poder agregar, consultar, modificar y eliminar un producto y manejarlo en persistencia de archivos (basado en entregable 1).
+ - La clase debe contar con una variable this.path, el cual se inicializará desde el constructor y debe recibir la ruta a trabajar desde el momento de generar su instancia.
+ - Debe guardar objetos con el siguiente formato:
+    * title (nombre del producto)
+    * description (descripción del producto)
+    * price (precio)
+    * thumbnail (ruta de imagen)
+    * code (código identificador)   
+    * stock (número de piezas disponibles)
+- Debe contar con un método “addProduct” el cual agregará un producto al arreglo de productos inicial (Validar que no se repita el campo “code” y que todos los campos sean obligatorios. Al agregarlo, debe crearse con un id autoincrementable).
+- Debe contar con un método “getProducts” el cual debe devolver el arreglo con todos los productos creados hasta ese momento.
+- Debe contar con un método “getProductById” el cual debe buscar en el arreglo el producto que coincida con el id (En caso de no coincidir ningún id, mostrar en consola un error “Not found”).
+- Debe tener un método updateProduct, el cual debe recibir el id del producto a actualizar, así también como el campo a actualizar (puede ser el objeto completo, como en una DB), y debe actualizar el producto que tenga ese id en el archivo. NO DEBE BORRARSE SU ID.
+- Debe tener un método deleteProduct, el cual debe recibir un id y debe eliminar el producto que tenga ese id en el archivo.
+
+
+
+
+
---
+++ Entregable_Clase_04/main.js
@@ -0,0 +1,191 @@
+const fs = require('fs');
+
+// CLASE ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class ProductManager {
+    constructor(path) {
+      this.products = [];
+      this.path = path;
+      this.lastProductId = 0;
+      this.loadProducts();
+
+    }
+
+    loadProducts() {
+      try {
+        const data = fs.readFileSync(this.path);
+        this.products = JSON.parse(data);
+        this.lastProductId = this.products.reduce((maxId, product) => Math.max(maxId, product.id), 0);
+      } catch (error) {
+        this.products = [];
+        this.lastProductId = 0;
+      }
+    }
+
+    saveProduct(){
+      try{
+          fs.writeFileSync(this.path, JSON.stringify(this.products, null, 2))
+      } catch(err){
+          console.log('Error al guardar', err)
+      }
+    }
+
+    generateCode() {
+        this.lastProductId++;
+        return this.lastProductId;
+    }
+  
+    getProducts() {
+      try {
+        return this.products;
+      } catch (error) {
+        console.log('Error al buscar productos', error)
+      }
+    }
+  
+    addProduct(title, description, price, thumbnail, stock) {
+
+      if (!title || !description || !price || !thumbnail || !stock) {
+        throw new Error("Todos los campos son obligatorios.");
+       }
+    
+      if (this.products.some(existingProduct => existingProduct.code === code)) {
+        throw new Error("El código del producto ya existe.");
+      }
+
+      const code = this.generateCode();
+
+      const product = { title, description, price, thumbnail, code, stock };
+
+      this.products.push(product);
+      
+      this.saveProduct;
+
+      return product;
+    }
+  
+    getProductById(id) {
+      const product = this.products.find(p => p.code === id); 
+      if (product) {
+        return product;
+      } else {
+        throw new Error("Producto no encontrado");
+      }
+    }
+
+    updateProduct(id, update){
+      
+      const product = this.products.findIndex(p => p.code === id);
+        if (product !== -1) {
+          const updatedProduct = Object.assign({}, this.products[product], update);
+          this.products[product] = updatedProduct;
+          this.saveProduct();
+          console.log(`El producto con el id ${id} fue actualizado`);
+          return updatedProduct;
+        }else{
+          throw new Error("Producto no se pudo actualizar");
+        }
+    }
+
+    deleteProduct(id){
+      try {
+          const product = this.products.findIndex(p => p.code === id);
+          this.products.splice(product,1);
+          this.saveProduct();
+          console.log(`El producto con el id ${id} fue borrado`)
+          
+      } catch (err) {
+          console.log('Error al borrar producto', err)
+      }
+    }
+  }
+  
+
+// TEST ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+const manager = new ProductManager("productos.json");
+
+console.log("1. Instancia creada:", manager);
+
+const products = manager.getProducts();
+
+console.log("2. Resultado de getProducts:", products);
+
+try {
+
+  product1 = manager.addProduct("producto 1", "Este es el producto de prueba 1", 100, "sin imagen", 10);
+  product2 = manager.addProduct("producto 2", "Este es el producto de prueba 2", 150, "sin imagen", 15);
+  product3 = manager.addProduct("producto 3", "Este es el producto de prueba 3", 200, "sin imagen", 20);
+  product4 = manager.addProduct("producto 4", "Este es el producto de prueba 4", 250, "sin imagen", 25);
+  product5 = manager.addProduct("producto 5", "Este es el producto de prueba 5", 300, "sin imagen", 30);
+
+  console.log("3. Productos agregados");
+
+} catch (error) {
+
+  console.log("3. Error al agregar productos:", error.message);
+
+}
+
+const productsAfterAdding = manager.getProducts();
+
+console.log("4. Productos después de agregar uno:", productsAfterAdding);
+
+try {
+
+  const product6 = manager.addProduct("producto repetido", "Este es un producto repetido", 150, "Otra imagen",1, 15);
+
+  console.log("5. Producto repetido agregado:", product6);
+
+} catch (error) {
+
+  console.log("5. Error al agregar producto repetido:", error.message);
+
+}
+
+try {
+
+  const productFound = manager.getProductById(1);
+
+  console.log("6. Producto encontrado:", productFound);
+
+} catch (error) {
+
+  console.log("6. Error al buscar producto:", error.message);
+
+}
+
+try {
+
+  const productNotFound = manager.getProductById(90);
+
+  console.log("6. Producto no encontrado:", productNotFound);
+
+} catch (error) {
+
+  console.log("6. Error al buscar producto no encontrado:", error.message);
+
+}
+
+try {
+  const updateProduct = manager.updateProduct(1, {title: "producto con el titulo editado", thumbnail: "sin imagen pero editado"});
+
+  console.log("7. Producto editado:", updateProduct);
+
+} catch (error) {
+
+  console.log("7. Error al editar producto:", error.message);
+
+}
+
+try {
+  
+  const deletedProduct = manager.deleteProduct(2);
+
+  console.log("8. Producto borrado con exito: ", deletedProduct)
+
+} catch (error) {
+
+  console.log("8. Error al borrar producto:", error.message);
+
+}
