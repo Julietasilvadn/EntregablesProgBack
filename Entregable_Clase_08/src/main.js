6bbe51503bc742a6fed776c09728536211e948a3
Julieta Silva <juuli.siilva@gmail.com>
2023-11-27 21:30:40 -0300

    Entregable clase8

---
+++ Entregable_Clase_08/src/main.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { rutaProducto } from './rutas/productos.js';
+import { rutaCarrito } from './rutas/carrito.js';
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//JSON----------------------------------------------------------------------------------------------------------------
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//RUTAS----------------------------------------------------------------------------------------------------------------
+app.use('/api/products', rutaProducto);
+app.use('/api/carts', rutaCarrito);
+
+//MIDLEWARE----------------------------------------------------------------------------------------------------------------
+app.use((peticion, respuesta, next) => {
+  if (!peticion.route) {
+    respuesta.status(404).send({ error : -2, descripcion: `ruta ${peticion.url} no encontrada` });
+  } else {
+    next();
+  }
+})
+
+// SERVIDOR ----------------------------------------------------------------------------------------------------------------
+const servidor = app.listen(PORT, () => {
+    console.log(`Servidor escuchando: ${PORT}`);
+  });
+  
+  servidor.on('error', error => console.log(`Error: ${error}`));
